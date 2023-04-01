d42bd50664d43451a50f8523242dd9cfda1a5bba
Thibaut de Saivre <thibaut.de-saivre@polytechnique.edu>
2023-04-01 19:01:30 +0200

    added script to compute flow cost
    Former-commit-id: 345f783771672783ee0a07c6c340ca22ec84de1d

---
+++ compute_flow_cost.py
@@ -0,0 +1,31 @@
+"""
+Compute the total cost of a flow
+
+Current method :
+
+Cost for an edge : distance + x
+Cost for a connection : 5000 + x
+"""
+import numpy as np
+from typing import List
+from util.util import read_json, Network
+
+if __name__ == "__main__":
+    
+    # Read network data
+    edge_distances: List = read_json("edge_distances.json")
+    network : Network = read_json("paris_network.json")
+
+    total_len = len(network["edges"]) + len(network["metro_connections"]) + len(network["rer_connections"]) + len(network["trans_connections"])
+
+    b_coeffs = np.array(edge_distances + [5000] * (total_len - len(edge_distances)))
+    a_coeffs = np.ones(total_len)
+
+
+    # Read flow data
+    flow = np.array(read_json("optimized_paths_last_flow.json"))
+
+    total_cost = np.sum(a_coeffs * flow + b_coeffs)
+
+    print("Total cost for this flow :")
+    print(total_cost)
