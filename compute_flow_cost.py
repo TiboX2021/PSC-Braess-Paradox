44856ddfbaa2668d3606b8726323406db5fffc2b
Thibaut de Saivre <thibaut.de-saivre@polytechnique.edu>
2023-04-10 22:06:49 +0200

    Caching to speedup subsequent calls
    Former-commit-id: c11f172ce2379d5b998e0fa633d1904e0f660f5d

--- compute_flow_cost.py
+++ compute_flow_cost.py
@@ -12,9 +12,15 @@ import numpy as np
 
 from util.util import read_json, Network, read_argv
 
+# Add caching to speedup subsequent calls
+__b_coeffs = None
+__a_coeffs = None
+
 
 def compute_flow_cost(flow: np.ndarray):
     """Compute the total cost for a flow"""
+    global __b_coeffs, __a_coeffs
+
     # Read network data
     edge_distances: List = read_json("edge_distances.json")
     network: Network = read_json("paris_network.json")
@@ -22,11 +28,12 @@ def compute_flow_cost(flow: np.ndarray):
     total_len = len(network["edges"]) + len(network["metro_connections"]) + len(network["rer_connections"]) + len(
         network["trans_connections"])
 
-    b_coeffs = np.array(edge_distances + [5000] * (total_len - len(edge_distances)))
-    a_coeffs = np.ones(total_len)
+    if __b_coeffs is None or __a_coeffs is None:
+        __b_coeffs = np.array(edge_distances + [5000] * (total_len - len(edge_distances)))
+        __a_coeffs = np.ones(total_len)
 
     # IMPROVEMENT : only add the b_coeff where there are people? Else this is just a constant cost...
-    total_cost = np.sum(a_coeffs * flow * flow + b_coeffs * flow)  # En réalité il faudrait écrire ça comme ça !
+    total_cost = np.sum(__a_coeffs * flow * flow + __b_coeffs * flow)  # En réalité il faudrait écrire ça comme ça !
     return total_cost
 
 
