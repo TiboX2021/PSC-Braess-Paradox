fd861dfe039db7c1042481a2f808c86e788c2d40
Thibaut de Saivre <thibaut.de-saivre@polytechnique.edu>
2023-04-10 12:24:30 +0200

    complete correctness benchmark
    Former-commit-id: 54cc860cd86e72f47c1a36fd153e5dd073bdf714

--- benchmark_graphs.py
+++ benchmark_graphs.py
@@ -24,7 +24,7 @@ from franck_wolfe import Paris
 from util.util import read_json
 
 
-def test_benchmark():
+def benchmark_correctness_for_n_paths():
     """Test first benchmark from the benchmark list
     The goal is to show correctness for least than 5 different paths"""
     passengers = 1000
@@ -33,20 +33,34 @@ def test_benchmark():
     destinations = [(i * 100, (i + 1) * 100, passengers) for i in range(5)]
 
     n_values = [2, 3, 4, 5, 6, 7]
-    flow_costs = []
+    flow_costs1 = []
 
     for i, n in enumerate(n_values):
         print("Test", i + 1, "of", len(n_values))
         flow = p.solve_paths(n, destinations, False)
-        flow_costs.append(compute_flow_cost(flow))
+        flow_costs1.append(compute_flow_cost(flow))
 
-    # Normalize cost
-    max_cost = max(flow_costs)
-    print(max_cost)
-    # flow_costs = [value / max_cost for value in flow_costs]
+    # Generate 10 destinations
+    destinations = [(i * 50, (i + 1) * 50, passengers) for i in range(10)]
+    flow_costs2 = []
+
+    for i, n in enumerate(n_values):
+        print("Test", i + 1 + len(n_values), "of", len(n_values) * 2)
+        flow = p.solve_paths(n, destinations, False)
+        flow_costs2.append(compute_flow_cost(flow))
+
+    # Normalize costs
+    flow_costs1 = [value / max(flow_costs1) for value in flow_costs1]
+    flow_costs2 = [value / max(flow_costs2) for value in flow_costs2]
 
     # Print flows
-    plt.plot(n_values, flow_costs)
+    plt.title("Correctness benchmark - paths per destination")
+    plt.plot(n_values, flow_costs1, label='5 couples')
+    plt.plot(n_values, flow_costs2, label='10 couples')
+    plt.xlabel("Paths per couple")
+    plt.ylabel('Total flow cost (UA)')
+    plt.legend()
+    plt.grid()
     plt.show()
 
 
@@ -57,4 +71,4 @@ if __name__ == "__main__":
 
     p = Paris(graph_data, edge_distances)
 
-    test_benchmark()
+    benchmark_correctness_for_n_paths()
